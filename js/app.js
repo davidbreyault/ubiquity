21225f04389bb9629309f552854eb97449caafff
David Breyault <davidbreyault@gmail.com>
2021-04-11 22:45:10 +0200

    Amélioration de la navigation à la tabulation (avec visibility)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 const burgerButton = document.querySelector('.btn');
 const headerMenu = document.querySelector('.header-navbar');
+const headerMenuLinks = document.querySelectorAll('.header-navbar a');
 const arrowsSlider = document.querySelector('.arrows-container .container');
 
 function showArrowsSlider() {
     window.innerWidth >= 992 ? arrowsSlider.style.display = 'flex' : arrowsSlider.style.display = 'none';
+}
+
+function linksVisibility() {
+    if (window.innerWidth < 768) {
+        headerMenuLinks.forEach((link) => {
+            link.style.visibility = 'hidden';
+        })
+        burgerButton.addEventListener('click', () => {
+            headerMenuLinks.forEach((link) => {
+                link.style.visibility = 'visible';
+            })    
+        })
+    }
 }
 
 burgerButton.addEventListener('click', () => {
@@ -20,4 +34,4 @@ window.addEventListener('resize', () => {
 })
 
 showArrowsSlider();
-
+linksVisibility();
